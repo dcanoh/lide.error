7ab974aa2435560676e7b6876be0cfca3557e858
Hernan Dario Cano <dcanohdev@gmail.com>
2017-12-03 23:35:07 -0500

    Add ltrace for traceback formatting
    With this commit implements ltrace to print more complex tracebacks
    when debug lua code, ltrace acts as old lperr function that gots
    tracebak info as string.
    
    To see current changes you can throw an Exception with:
    
    	lide.error.TypeException ('bad value');

--- lua/lide/error/init.lua
+++ lua/lide/error/init.lua
@@ -1,26 +1,26 @@
 assert( class or lide.class, "liderror: requires lide.class" )
 
---local lua = { error = error }
+local ltrace = require 'lide.error.ltrace'
 
 --====================================================================--
 --== Setup, Constants
 
 local DEFAULT_PREFIX     = "ERROR: "
 local DEFAULT_MESSAGE    = "LuaException"
+local DEFAULT_STACKLEVEL = 3
+local REM_NUM_STACKS     = 4
 
 --====================================================================--
 --== Support Functions
 
 -- based on https://gist.github.com/cwarden/1207556
 
-
 local function try( funcs )
 	local try_f, catch_f, finally_f = funcs[1], funcs[2], funcs[3]
 	assert( try_f, "lua-error: missing function for try()" )
-	--==--
 	
-	lide.try_level = 6
-
+	REM_NUM_STACKS = 6
+	
 	local status, result = pcall(try_f)
 	if not status and catch_f then
 		catch_f(result)
@@ -34,124 +34,19 @@ local function catch(f)
 end
 
 local function finally(f)
+	REM_NUM_STACKS = nil
 	return f[1]
 end
 
 --====================================================================--
 --== Error Base Class
 --====================================================================--
---local Object = lide.classes.object
-local Error = class 'Error' --: subclassof 'Object' : global ( false )
+
+local Error = class 'Error' : subclassof ( Object )
 
 local DEFAULT_ERROR_NAME = Error:name()
 
-local lperr
-
----- lide print error
-lperr = function ( sErrMsg, nlevel )
-		local short_src
-		
-		lide.core.base.isstring(sErrMsg)
-		
-
-		nlevel = nlevel or 1
-		-- levels { 
-		--	 1 = the function itself
-		-- 	 2 = error dispatcher
-		--   3 = caller of error_dispacher()
-		--   4 = caller of 3
-		--	 ..= main()
-		-- }	
-		local print_ok = true
-		
-		local function print( ... )
-			io.stderr:write( tostring(unpack{...} or '') .. '\n')
-		end
-		
-		--local err_msg = ('Lide, Error: %s\n\n'):format(sErrMsg)
-		local err_msg = ''
-		
-		local tmp_file_src = '' -- Esto es para que todos los levels continuos se junten:
-		-- [/fs/ss/loo.lua]
-		-- [line:1] dadasdsada
-		-- [line:104] dadasdsada
-		--
-		-- Comienza desde 2: "la funcion que genera el error":
-		-- repeat nlevel  = nlevel +1 (...)
-		local traceback = ''
-		local i = nlevel repeat i = i +1
-			local level = debug.getinfo(i)
-			if level then
-				local short_src   = tostring(level.short_src or 'NULL_FILE')
-				local currentline = tonumber(level.currentline or -3) --> -3 simplemnte es para identificar que es un error, este numero no tiene nada que ver con nada.
-				local namewhat    = tostring(level.namewhat or 'NULL_NAMEWHAT')
-				local name        = tostring(level.name or 'NULL_NAME')
-				local print_line  = true
-
-				if level.what == 'main' then
-					name     = 'main'
-					namewhat = 'chunk'
-				end
-				
-				if name == 'NULL_NAME' or name == 'NULL_NAME ' and level.what == 'main' then
-					name = '..main chunk..'
-					print_line = false
-				end
-				
-				if currentline == -1 and name == 'pcall' then
-					print_line = false
-				end
-
-				if short_src == '[string ""]' then
-					--print(namewhat)
-					--print(name)
-					--print(currentline)
-					print_line = false
-				end
-
-
-
-				if (namewhat == 'global') and (type(level.func) == 'function') then
-					namewhat = 'global function definition'
-				elseif (namewhat == 'upvalue') or (namewhat == 'local') and (type(level.func) == 'function') then
-					namewhat = 'local function definition'
-				elseif (namewhat == 'method') and ( name == 'init' ) and (type(level.func) == 'function') then
-					local _, self = debug.getlocal(i, 1) -- get 'self'
-					name = 'constructor '
-					namewhat = ('definition of "%s"'):format(tostring(self:class() or ''))
-				elseif (namewhat == 'method') and (type(level.func) == 'function') then
-					local _, self, class_name = debug.getlocal(i, 1) -- get 'self'
-					
-					if self and getmetatable(self) and getmetatable(self).__lideobj and not self.getName 
-						and type(getmetatable(self).__index) == 'table' then 
-						local class_name = getmetatable(self).__index.name()
-						if  getmetatable(self).__lideobj and getmetatable(self).__type == 'class' then						
-							print_line = false --> no imprimir estalinea en el traceback --[./lide/core/oop/yaci.lua]
-						end
-					else
-						if self and getmetatable(self) and getmetatable(self).__lideobj and getmetatable(self).__type ~= 'class' then
-							-- Obtenemos el nombre de la clase desde yaci:
-							class_name = self:class():name()
-						end
-					end
-					namewhat = ('method of "%s" object'):format(class_name or '')
-				end
-
-				if print_line then
-					if (tmp_file_src ~= short_src) then 
-						traceback = (traceback .. '[%s]\n' ):format(short_src)
-					end
-					traceback = traceback.. ('[line:%d] in %s %s.\n'):format(currentline, name, namewhat)
-				end
-			end			
-			tmp_file_src = short_src ;
-		until not debug.getinfo(i) or debug.getinfo(i).what == 'main'
-
-		return (err_msg .. traceback) --os.exit()
-end
-
-
-Exception = class 'Exception' --: subclassof 'Object' : global ( false )
+Exception = class 'Exception' : subclassof ( Object )
 
 function Exception:Exception ( sExceptionName )
    public {
@@ -168,19 +63,19 @@ function Exception:Exception ( sExceptionName )
    self.super : init (self.name)
 end
 
-function Exception:__call ( message, nlevel )
-	self.nlevel    = nlevel or lide.try_level or 2
+function Exception:__call ( message, nlevel )	
+	self.nlevel       = nlevel or DEFAULT_STACKLEVEL
 	
-	self.message   = message or self.message
-	self.traceback = lperr (self.message, self.nlevel)
+	local maxstack    = (#ltrace.getstack(self.nlevel) - REM_NUM_STACKS)
 	
-	lide.core.lua.error (self, self.nlevel)
-
-	--return self
+	self.message   = 'TypeException: ' .. (message or self.message)
+	self.traceback = self.message ..'\n\n'.. ltrace.getfulltrace(self.nlevel, maxstack)
+	
+	lide.core.lua.error (self)
 end
 
 function Exception:__tostring( ... )
-	return table.concat( { self.message, "\n", self.traceback } )
+	return self.traceback
 end
 
 function Exception:isa ( ExceptionObject )
@@ -188,11 +83,10 @@ function Exception:isa ( ExceptionObject )
 end
 
 function Error:Error ( message, nlevel )
-	--params = params or {}
 	
 	public { 
 		message = message or DEFAULT_MESSAGE,
-		nlevel = nlevel or 1
+		nlevel  = nlevel or DEFAULT_STACKLEVEL
 	}
 	
 	self . super :init 'LideException'
@@ -200,15 +94,25 @@ function Error:Error ( message, nlevel )
 	lide.core.lua.error(self.message, self.nlevel)
 end
 
---function Error:getExceptionName( ... )
---	return self.__exception
---end
-
--- must return a string
---
----Error:virtual '__tostring'
 function Error:__tostring( ... )
 	return table.concat( { self.message, "\n", self.traceback } )
+end
+
+local TypeException = Exception :new 'TypeException'
+
+
+local function istype( ValueToCompare, TypeToCompare )
+	local errmsg = errmsg or ('type is incorrect, must be %s.'):format(TypeToCompare)
+	
+	if lide.core.base.type(ValueToCompare) == TypeToCompare then 
+		return ValueToCompare 
+	else 
+		if not errmsg then 
+			return false ;
+		else 
+			TypeException(errmsg, DEFAULT_STACKLEVEL +1);
+		end 
+	end
 end
 
 Error : enum { 
@@ -218,7 +122,33 @@ Error : enum {
    	return newException
 	end ,
 
-	--Exception = Exception
+	DEFAULT_STACKLEVEL = DEFAULT_STACKLEVEL,
+
+	TypeException = TypeException,
+
+	is_number = function ( value, errmsg )
+		return istype(value, 'number');
+	end,
+
+	is_string = function ( value, errmsg )
+		return istype(value, 'string');
+	end,
+
+	is_function = function ( value, errmsg )
+		return istype(value, 'function');
+	end,
+
+	is_object = function ( value, errmsg )
+		return istype(value, 'object');
+	end,
+
+	is_boolean = function ( value, errmsg )
+		return istype(value, 'boolean');
+	end,
+
+	is_table = function ( value, errmsg )
+		return istype(value, 'table');
+	end,
 }
 
 --====================================================================--
